6f3063fabd7b6fa97b36b1d77246196c5cfd9096
Hrithik Gupta <hrithikgupta52@gmail.com>
2024-04-30 15:36:46 +0530

    adding components

--- src/TaskTrackerApp.js
+++ src/TaskTrackerApp.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function TaskTrackerApp() {
-    const [allTask, setAllTask] = useState([])
+  const [allTask, setAllTask] = useState([]);
   return (
-    <div>TaskTrakerApp</div>
-  )
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <h3>Task Tracker</h3>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          {/* SideBar */}
+        </Grid>
+        <Grid item xs={8}>
+          <TaskList />
+          <TaskForm />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default TaskTrackerApp
+export default TaskTrackerApp;
