41760ae0155a9a6d614f38291c7f35d62ab968eb
Hrithik Gupta <hrithikgupta52@gmail.com>
2024-04-30 15:01:24 +0530

    initial file setup

---
+++ src/TaskTrakerApp.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function TaskTrakerApp() {
+    const [allTask, setAllTask] = useState([])
+  return (
+    <div>TaskTrakerApp</div>
+  )
+}
+
+export default TaskTrakerApp
